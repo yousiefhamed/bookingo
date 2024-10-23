7b528d7122183a1a149296617df2df53a234f684
Youssef Hamed <yousiefhamed1@gmail.com>
2024-10-23 19:12:09 +0300

    popular books

--- src/components/PopularBooks.jsx
+++ src/components/PopularBooks.jsx
@@ -1,49 +1,26 @@
 import Headeing from "./utils/Headeing";
 import Book from "./BookCard";
 
-import test from "./../assets/books/test.png";
+import { useAppContext } from "../context/AppContext";
+import { useEffect, useState } from "react";
 
 const PopularBooks = () => {
-  const books = [
-    {
-      id: 1,
-      title: "The Great Gatsby",
-      author: "F. Scott Fitzgerald",
-      image: test,
-    },
-    {
-      id: 2,
-      title: "To Kill a Mockingbird",
-      author: "Harper Lee",
-      image: test,
-    },
-    {
-      id: 3,
-      title: "1984",
-      author: "George Orwell",
-      image: test,
-    },
-    {
-      id: 4,
-      title: "Pride and Prejudice",
-      author: "Jane Austen",
-      image: test,
-    },
-    {
-      id: 5,
-      title: "The Catcher in the Rye kafja kfjafk lajfak",
-      author: "J.D. Salinger",
-      image: test,
-    },
-  ];
+  const { books } = useAppContext();
+  const [filteredBooks, setFilteredBooks] = useState([]);
+
+  useEffect(
+    () =>
+      setFilteredBooks(books.sort((a, b) => b.rating - a.rating).slice(0, 5)),
+    [books]
+  );
 
   return (
     <section className="popular-books">
       <Headeing text="Popular Books" />
       <div className="books">
-        {books.map((book) => (
-          <Book key={book.id} book={book} />
-        ))}
+        {filteredBooks.length === 0
+          ? ""
+          : filteredBooks.map((book) => <Book key={book._id} book={book} />)}
       </div>
     </section>
   );
